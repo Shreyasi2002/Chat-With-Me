c809faa6f5fdb5662ef4d68cd35697a2a16cff42
Shreyasi2002 <shreyasi18sia@gmail.com>
2021-10-24 21:23:37 +0530

    Responsive Sidebar with Media Query

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -4,18 +4,25 @@ import React from 'react';
 import { Dashboard } from '@rsuite/icons';
 
 import { Button, Drawer } from 'rsuite';
-import { useModalState } from '../../misc/custom-hooks';
+import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
 
 import DashboardShow from '.';
 
 const DashboardToggle = () => {
     const { isOpen, open, close } = useModalState();
+    const isMobile = useMediaQuery('(max-width: 992px)');
+
     return (
         <>
             <Button block color="blue" appearance="primary" onClick={open}>
                 <Dashboard /> Dashboard
             </Button>
-            <Drawer open={isOpen} onClose={close} placement="left">
+            <Drawer
+                full={isMobile}
+                open={isOpen}
+                onClose={close}
+                placement="left"
+            >
                 <DashboardShow />
             </Drawer>
         </>
