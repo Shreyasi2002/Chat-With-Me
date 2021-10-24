356ad420fcf4bb63e446118fb77c60c43415e9ca
Shreyasi2002 <shreyasi18sia@gmail.com>
2021-10-24 13:17:18 +0530

    Public and Private Routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Switch } from 'react-router';
 
 import 'rsuite/dist/rsuite.min.css';
+import PrivateRouter from './components/PrivateRouter';
+import PublicRouter from './components/PublicRouter';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
 import './styles/main.scss';
 
 function App() {
-    return <div>hello</div>;
+    return (
+        <Switch>
+            <PublicRouter path="/signin">
+                <SignIn />
+            </PublicRouter>
+            <PrivateRouter path="/">
+                <Home />
+            </PrivateRouter>
+        </Switch>
+    );
 }
 
 export default App;
---
+++ src/components/PrivateRouter.js
@@ -0,0 +1,14 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+import { Redirect, Route } from 'react-router';
+
+const PrivateRouter = ({ children, ...routeProps }) => {
+    const profile = false;
+
+    if (!profile) {
+        return <Redirect to="/signin" />;
+    }
+    return <Route {...routeProps}>{children}</Route>;
+};
+
+export default PrivateRouter;
